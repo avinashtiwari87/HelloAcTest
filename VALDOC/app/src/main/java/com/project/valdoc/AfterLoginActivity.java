cf3b7d93824abf974dcdb4aeb594517f59b44e43
avinash <avinashtiwari87@gmail.com>
2016-03-08 01:50:48 +0530

    test

--- VALDOC/app/src/main/java/com/project/valdoc/AfterLoginActivity.java
+++ VALDOC/app/src/main/java/com/project/valdoc/AfterLoginActivity.java
@@ -12,6 +12,7 @@ public class AfterLoginActivity extends AppCompatActivity {
     private String loginUserType = "";
     private int appUserId;
 
+    //?
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,19 +27,19 @@ public class AfterLoginActivity extends AppCompatActivity {
             appUserId = extras.getInt("APPUSERID");
         }
 
-        if(getIntent().hasExtra("serviceReport")){
+        if (getIntent().hasExtra("serviceReport")) {
             getSupportActionBar().hide();
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
             setContentView(R.layout.service_report_entry);
 
-        }else{
+        } else {
             setContentView(R.layout.activity_after_login);
 
 
             findViewById(R.id.service_report_btn).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(AfterLoginActivity.this,AfterLoginActivity.class);
+                    Intent intent = new Intent(AfterLoginActivity.this, AfterLoginActivity.class);
                     intent.putExtra("serviceReport", true);
                     //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
@@ -49,10 +50,10 @@ public class AfterLoginActivity extends AppCompatActivity {
             findViewById(R.id.test_certificate_btn).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent=new Intent(AfterLoginActivity.this, TestCreateActivity.class);
-                    intent.putExtra("USERNAME",userName);
-                    intent.putExtra("USERTYPE",loginUserType);
-                    intent.putExtra("APPUSERID",appUserId);
+                    Intent intent = new Intent(AfterLoginActivity.this, TestCreateActivity.class);
+                    intent.putExtra("USERNAME", userName);
+                    intent.putExtra("USERTYPE", loginUserType);
+                    intent.putExtra("APPUSERID", appUserId);
                     startActivity(intent);
                 }
             });
