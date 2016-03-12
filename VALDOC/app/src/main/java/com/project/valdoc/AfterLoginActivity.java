7fe0343504f78b4a96939ec06430faa55f0497fc
avinash <avinashtiwari87@gmail.com>
2016-03-12 11:00:56 +0530

    screen navigation back pressed overriding

--- VALDOC/app/src/main/java/com/project/valdoc/AfterLoginActivity.java
+++ VALDOC/app/src/main/java/com/project/valdoc/AfterLoginActivity.java
@@ -1,8 +1,11 @@
 package com.project.valdoc;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -76,4 +79,18 @@ public class AfterLoginActivity extends AppCompatActivity {
 
         }
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        new AlertDialog.Builder(this)
+//                .setTitle("Really Exit?")
+//                .setMessage("Are you sure you want to exit?")
+//                .setNegativeButton(android.R.string.no, null)
+//                .setPositiveButton(android.R.string.yes, new OnClickListener() {
+//
+//                    public void onClick(DialogInterface arg0, int arg1) {
+//                        AfterLoginActivity.super.onBackPressed();
+//                    }
+//                }).create().show();
+//    }
 }
