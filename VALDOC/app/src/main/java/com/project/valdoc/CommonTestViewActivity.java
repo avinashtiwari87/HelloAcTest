5d4de55d8631a7bdfed8fa8fc2de77169f6bcef6
Saurabh Sharma <saurabh.sharma569@gmail.com>
2016-06-20 23:33:18 +0530

    Fixed Footer Issue in CommonTestView Screen

--- VALDOC/app/src/main/java/com/project/valdoc/CommonTestViewActivity.java
+++ VALDOC/app/src/main/java/com/project/valdoc/CommonTestViewActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -880,6 +881,8 @@ public class CommonTestViewActivity extends AppCompatActivity {
 
     private void initRes() {
         findViewById(R.id.header4_linear_layout).setVisibility(View.GONE);
+        findViewById(R.id.submit).setVisibility(View.GONE);
+        findViewById(R.id.clear).setVisibility(View.GONE);
         //Test 1
         table_layout = (TableLayout) findViewById(R.id.tableLayout1);
         table_layout2 = (TableLayout) findViewById(R.id.tableLayout2);
