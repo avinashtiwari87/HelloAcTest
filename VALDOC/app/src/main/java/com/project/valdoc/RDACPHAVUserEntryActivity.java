c1f6a1f767440dd65f64b11021f52a484a5df911
Saurabh Sharma <saurabh.sharma569@gmail.com>
2017-04-06 11:54:20 +0530

    Fixed Airflow heading inside v1 v2

--- VALDOC/app/src/main/java/com/project/valdoc/RDACPHAVUserEntryActivity.java
+++ VALDOC/app/src/main/java/com/project/valdoc/RDACPHAVUserEntryActivity.java
@@ -15,6 +15,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
